3cb1771a5150fb4152dc0ec5a21979a04b8680aa
Ivan Alves <ivan.alves22@hotmail.com>
2023-07-21 09:57:08 -0300

    Added products cards list with toast message."

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    //unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "store.storeimages.cdn-apple.com",
+        port: "",
+        pathname: "/4982/as-images.apple.com/is/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
+import Header from "@/src/components/Header";
 import Head from "next/head";
+import Link from "next/link";
+import { Button, Container } from "reactstrap";
 
 export default function Home() {
   return (
@@ -9,17 +12,26 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint error
-          consectetur modi odio obcaecati tempore dignissimos assumenda, eum
-          reiciendis dolores magni autem at debitis voluptas. Rem ipsa deleniti
-          autem dignissimos.
-        </p>
+      <Header />
+
+      <main>
+        <Container className="py-5 text-center">
+          <h1 className="mt-5 display-1">
+            O melhor jeito de comprar o que você ama
+          </h1>
+          <p className="my-4">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae
+            iusto voluptatem obcaecati omnis error architecto neque cum
+            exercitationem fugiat. Vero illo autem eum nisi sapiente, odio optio
+            accusamus cupiditate ad.
+          </p>
+          <Link href="/products">
+            <Button color="dark" className="px-4 pb-2">
+              Conheça nossos produtos!
+            </Button>
+          </Link>
+        </Container>
       </main>
     </>
   );
