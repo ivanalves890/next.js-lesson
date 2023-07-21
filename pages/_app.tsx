3917743e7d6f619aa30a52191e8db01afe16d459
Ivan Alves <ivan.alves22@hotmail.com>
2023-07-21 10:51:02 -0300

    Added cart functionalities.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-
 import type { AppProps } from "next/app";
+import { CartContextProvider } from "@/src/hooks/useCart";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <CartContextProvider>
+      <Component {...pageProps} />
+    </CartContextProvider>
+  );
 }
